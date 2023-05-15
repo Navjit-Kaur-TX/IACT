c2d475240185a029595e86cadfe736998ab3244a
Manisha148 <manishaverma70241@gmail.com>
2023-05-15 14:15:43 +0530

    codepipeline

---
+++ Terraform-Codepipeline/variable.tf
@@ -0,0 +1,18 @@
+variable "s3_bucket_name" {
+  type = string
+}
+
+variable "source_code_zip_file_key" {
+  type = string
+}
+
+variable "codebuild_project_name" {
+  type = string
+}
+variable "codedeploy_app_name" {
+  type = string
+}
+
+variable "codedeploy_deployment_group_name" {
+  type = string
+}
